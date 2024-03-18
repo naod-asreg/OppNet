f43a59348416069a2aa26b7cbb086821ed65acff
Naod <naa224@lehigh.edu>
2024-03-18 14:36:51 -0400

    socket

--- server/index.js
+++ server/index.js
@@ -3,13 +3,23 @@ import { port, mongoURL } from "./config.js";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from 'cors';
-
+import { Server } from "socket.io"; // Import Socket.io
+import { createServer } from "http"; // Import http module
 
 import userRoutes from './routes/userRoutes.js';
 import chatRoutes from './routes/chatRoutes.js';
 import applicationRoutes from './routes/applicationRoutes.js';
 
 const app = express();
+const httpServer = createServer(app); // Create HTTP server
+const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  });
 
 mongoose.connect(mongoURL)
     .then( () => {
@@ -25,8 +35,16 @@ app.use('/chats', chatRoutes);
 app.use('/users', userRoutes);
 app.use('/application', applicationRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// Socket.io event handling
+io.on("connection", (socket) => {
+    socket.on("join chat", ({ room, userId }) => { // Receive an object with room and userId
+        socket.join(room);
+        console.log(`User ${userId} Joined Room: ${room}`);
+    });
 });
 
 
+// Start the server
+httpServer.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
